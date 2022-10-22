ce3c75ef8e57a921b0dd6b875543e45952986e0a
Christian Ramundo <Christian.s.ramundo@gmail.com>
2022-10-22 18:54:41 -0300

    listos ambos ejercicios y funcionando

--- TareaJS/Logic.js/JSu4.js
+++ TareaJS/Logic.js/JSu4.js
@@ -1,13 +1,20 @@
+//Seleccion de botones por ID
 let red = document.querySelector("#red");
 let green = document.querySelector("#green");
 let blue = document.querySelector("#blue");
 let body = document.querySelector("#body");
-let h3Counter = document.querySelector("h3Counter");
+let h3PublicCounter = document.getElementById("#h3PublicCounter");
+let textArea = document.querySelector("#textArea4Count");
+let textInput = "";
 
 
 red.addEventListener("click",bodyRed);
 green.addEventListener("click",bodyGreen);
 blue.addEventListener("click",bodyBlue);
+textArea.addEventListener("keyup",storeInput);
+
+//Las funciones color agregan la clase del color correspondientes y eliminan
+//el resto en caso de que esten agregadas
 
 function bodyRed(){
     body.classList.toggle("colorRed")
@@ -26,4 +33,10 @@ function bodyBlue(){
     body.classList.toggle("colorBlue")
     body.classList.remove("colorGreen")
     body.classList.remove("colorRed")
+}
+
+function storeInput(){
+    textInput = textArea.value
+    h3PublicCounter = textInput.length
+    console.log(` La cantidad de caracteres tipeados es ${h3PublicCounter}`)
 }
