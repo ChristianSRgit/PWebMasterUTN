2982abff33800c6433cf63791174b0650135086b
Christian Ramundo <Christian.s.ramundo@gmail.com>
2022-10-22 13:03:13 -0300

    html listo, js elements select

---
+++ TareaJS/Logic.js/JSu4.js
@@ -0,0 +1,7 @@
+let red = document.querySelector("#red");
+let green = document.querySelector("#green");
+let blue = document.querySelector("#blue");
+let body = document.querySelector("body");
+let h3Counter = document.querySelector("h3Counter");
+
+
