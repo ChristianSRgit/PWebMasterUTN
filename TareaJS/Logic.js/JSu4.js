cff6ea855458b6e8aae7eec25fda36e48546101f
Christian Ramundo <Christian.s.ramundo@gmail.com>
2022-10-22 16:28:19 -0300

    cambio de colores del boddy con Toggle class listo

--- TareaJS/Logic.js/JSu4.js
+++ TareaJS/Logic.js/JSu4.js
@@ -1,7 +1,22 @@
 let red = document.querySelector("#red");
 let green = document.querySelector("#green");
 let blue = document.querySelector("#blue");
-let body = document.querySelector("body");
+let body = document.querySelector("#body");
 let h3Counter = document.querySelector("h3Counter");
 
 
+red.addEventListener("click",bodyRed);
+green.addEventListener("click",bodyGreen);
+blue.addEventListener("click",bodyBlue);
+
+function bodyRed(){
+    body.classList.toggle("colorRed")
+}
+
+function bodyGreen(){
+    body.classList.toggle("colorGreen")
+}
+
+function bodyBlue(){
+    body.classList.toggle("colorBlue")
+}
