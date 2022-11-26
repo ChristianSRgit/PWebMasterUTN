735727bbb0bc292fb327266bba7cba60024d7c9c
Christian Ramundo <Christian.s.ramundo@gmail.com>
2022-11-26 14:55:36 -0300

    auth error

--- MOD5U3/app.js
+++ MOD5U3/app.js
@@ -29,7 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 
-//select
+//selectt
 pool.query('select * from empleados').then(function 
   (resultados){
     console.log(resultados)
