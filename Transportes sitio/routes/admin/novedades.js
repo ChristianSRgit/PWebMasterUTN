30145cb6b7d770c414b7531df954b47692c7c4c5
christian Wirenet <christian.s.ramundo@gmail.com>
2022-12-27 16:20:28 -0300

    funnciones CRUD ok desde models

--- Transportes sitio/routes/admin/novedades.js
+++ Transportes sitio/routes/admin/novedades.js
@@ -21,5 +21,35 @@ router.get('/eliminar/:id', async (req, res, next) => {
     res.redirect('/admin/novedades')
 })
 
+//formulario de agregar novedades
+ router.get('/agregar', (req, res,next) => {
+    res.render('admin/agregar', {
+        layout: 'admin/layout'
+    })
+ });
+
+//insertar novedad
+router.post('/agregar', async (req, res, next) => {
+    try{
+        if(req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != ""){
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades')
+        } else {
+            res.render('admin/agregar', {
+                layout: 'admin/layout',
+                error:true,
+                message: 'Todos los campos son requeridos'
+            })
+        }
+    } catch (error){
+        console.log(error)
+        res.render('admin/agregar',{
+            layout: 'admin/layout',
+            error: true,
+            message: 'No se cargo la novedad'
+        })
+    }
+})
+
 
 module.exports = router;
