58b68c9ba18f331f44e4fbaea943984bef9c2bac
christian Wirenet <christian.s.ramundo@gmail.com>
2022-12-27 14:16:01 -0300

    novedades,login,logout,delete novedad ok

--- Transportes sitio/routes/admin/novedades.js
+++ Transportes sitio/routes/admin/novedades.js
@@ -1,12 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel');
 
+router.get('/', async function(req,res,next){
 
-router.get('/', function(req,res,next){
+    var novedades = await novedadesModel.getNovedades();
+
     res.render('admin/novedades', {
         layout: 'admin/layout',
         usuario: req.session.nombre,
+        novedades
     });
 });
+
+//eliminar novedades
+
+router.get('/eliminar/:id', async (req, res, next) => {
+    var id = req.params.id;
+    await novedadesModel.deleteNovedadesById(id);
+    res.redirect('/admin/novedades')
+})
+
 
 module.exports = router;
